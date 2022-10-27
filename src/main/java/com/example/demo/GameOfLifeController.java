ed15e7c4f562f6939ee8b961523bb0991ce48f3a
Simha <mor.simha@intel.com>
2022-10-27 17:26:56 +0300

    finished

--- src/main/java/com/example/demo/GameOfLifeController.java
+++ src/main/java/com/example/demo/GameOfLifeController.java
@@ -9,10 +9,11 @@ import javafx.scene.shape.Rectangle;
 import java.util.Random;
 
 public class GameOfLifeController {
-    final int SIZE = 100;
+    final int MAT_SIZE = 100;
+    final int SIDE = MAT_SIZE / 10;
     final int CELL_SIZE = 50;
-    Rectangle[][] matrix = new Rectangle[SIZE][SIZE];
-    Rectangle[][] nextGeneration = new Rectangle[SIZE][SIZE];
+    Rectangle[][] matrix = new Rectangle[MAT_SIZE][MAT_SIZE];
+    Rectangle[][] nextGeneration = new Rectangle[MAT_SIZE][MAT_SIZE];
     Random r = new Random();
     boolean firstRound = true;
     private GraphicsContext gc;
@@ -24,96 +25,104 @@ public class GameOfLifeController {
 
     public void initialize() {
         gc = cnvs.getGraphicsContext2D();
-        outlineGc = cnvs.getGraphicsContext2D();
+        outlineGc = cnvs.getGraphicsContext2D(); //this Graphics Context settings will define the outlines of each cell.
+        outlineGc.setFill(Color.BLACK);
+        outlineGc.setLineWidth(3);
     }
-
-    private void randomizeGame(int x, int y) {
-        if (r.nextInt(2) == 1) {
-            gc.setFill(Color.BLACK); //setting gc color
-            matrix [x][y].setFill(Color.BLACK);
-        } else {
-            gc.setFill(Color.WHITE);
-            matrix [x][y].setFill(Color.WHITE);
-        }
-    }
-
-    private int calcNextGen(int x, int y) {
-        int lifeCounter = 0;
-
-        for (int i = -1; i <= 1; i++)
-            for (int j = -1; j <= 1; j++) {
-                int newx = x + i;
-                int newy = y + j;
-               // System.out.println("x is " + x + " y is " + y + " newx is " + newx + " newy is " + newy);
-
-                if ((newx >= 0 && newy >= 0) && (newx <= 9 && newy <= 9)) {//positive
-                    if (newx != x || newy != y) {
-                        System.out.println("cord - (" + x + " , " + y + ") and check:  (" + newx + ", " + newy + ")");
-                        if ((matrix[newx][newy].getFill() == Color.WHITE))
-                            lifeCounter++;
-                   }
-                }
-            }
-        return lifeCounter;
-    }
-
-    private void paintNextGen (int lifeCounter, int x, int y) {
-        Paint currColor = matrix[x][y].getFill();
-
-        if (currColor == Color.WHITE) { //alive
-            if (lifeCounter == 2 || lifeCounter == 3) {
-                gc.setFill(Color.WHITE);
-                nextGeneration[x][y].setFill(Color.WHITE);
-            } else {
-                gc.setFill(Color.BLACK);
-                nextGeneration[x][y].setFill(Color.BLACK);
-            }
-        } else { //dead
-            if (lifeCounter == 3) { //back to life
-                gc.setFill(Color.WHITE);
-                nextGeneration[x][y].setFill(Color.WHITE);
-            }
-            else {
-                gc.setFill(Color.BLACK);
-                nextGeneration[x][y].setFill(Color.BLACK);
-            }
-        }
-    }
-
 
     @FXML
     protected void btnpressed() {
-        Random r = new Random();
-        outlineGc.setFill(Color.BLACK);
-        outlineGc.setLineWidth(3);
+        int lifeCounter;
 
-        for (int i = 0; i < SIZE/10; i+=1) {
-            for (int j = 0; j < SIZE / 10; j += 1) {
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
                 if (firstRound) {
                     matrix[i][j] = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
                     randomizeGame(i, j);
                     gc.fillRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //inside color
-                    outlineGc.strokeRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //outline color
                 } else {
-                    int lifeCounter = calcNextGen(i, j);
-                    System.out.println("this is lifeCounter for (" +i+","+j +"): "+lifeCounter);
                     nextGeneration[i][j] = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    lifeCounter = calcNextGen(i, j);
+                    System.out.println("this is lifeCounter for (" +i+","+j +"): "+lifeCounter);
                     paintNextGen(lifeCounter, i, j);
                     gc.fillRect(nextGeneration[i][j].getX(), nextGeneration[i][j].getY(), nextGeneration[i][j].getWidth(), nextGeneration[i][j].getHeight()); //inside color
-                    outlineGc.strokeRect(nextGeneration[i][j].getX(), nextGeneration[i][j].getY(), nextGeneration[i][j].getWidth(), nextGeneration[i][j].getHeight()); //outline color
                 }
-
+                outlineGc.strokeRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //outline color
             }
         }
-        if (!firstRound) {
-
-            for (int x = 0; x < SIZE / 10; x++)
-                System.arraycopy(nextGeneration[x], 0, matrix[x], 0, SIZE / 10);
-        }
-
+        if (!firstRound)
+            for (int x = 0; x < SIDE; x++)
+                System.arraycopy(nextGeneration[x], 0, matrix[x], 0, SIDE);
         firstRound = false;
     }
+
+    //Randomizes a number (0,1) and sets the color cell color by it.
+    // WHITE is dead and DIMGRAY is alive
+    private void randomizeGame(int x, int y) {
+        Color state;
+        if (r.nextInt(2) == 1) //Dead
+            state = Color.WHITE;
+        else //Alive
+            state = Color.DIMGRAY;
+
+        gc.setFill(state); //setting the cell's color.
+        matrix[x][y].setFill(state); //storing the color in the matrix
+    }
+
+    // checks cell's sibling color, to count lives around it.
+    private int calcNextGen(int x, int y) {
+        int lifeCounter = 0, newX, newY;
+        // checks all siblings options, beside the out of range ones.
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++) {
+                newX = x + i; //new x coordinate to check
+                newY = y + j;
+                if ((newX >= 0 && newY >= 0) && (newX < SIDE && newY < SIDE)) // check positive coordinate
+                    if (!(newX == x && newY == y)) // avoid check the middle one
+                        if ((matrix[newX][newY].getFill() == Color.DIMGRAY)) {
+                            lifeCounter++;
+                            System.out.println("cord - (" + x + " , " + y + ") and check:  (" + newX + ", " + newY + ")");
+                        }
+            }
+        return lifeCounter;
+    }
+
+    private void paintNextGen(int lifeCounter, int x, int y) {
+        Paint currColor = matrix[x][y].getFill();
+
+        if (lifeCounter == 3) { //3 siblings means a birth or existence
+            gc.setFill(Color.DIMGRAY);
+            nextGeneration[x][y].setFill(Color.DIMGRAY);
+        } else if (lifeCounter == 2 && currColor == Color.DIMGRAY) { //2 siblings for a living cell means existence
+            gc.setFill(Color.DIMGRAY);
+            nextGeneration[x][y].setFill(Color.DIMGRAY);
+        } else { //every other option means death
+            gc.setFill(Color.WHITE);
+            nextGeneration[x][y].setFill(Color.WHITE);
+        }
+    }
 }
+
+//
+//        if (currColor == Color.DIMGRAY) { //alive
+//            if (lifeCounter == 2 || lifeCounter == 3) {
+//                gc.setFill(Color.DIMGRAY);
+//                nextGeneration[x][y].setFill(Color.DIMGRAY);
+//            } else {
+//                gc.setFill(Color.WHITE);
+//                nextGeneration[x][y].setFill(Color.WHITE);
+//            }
+//        } else { //dead
+//            if (lifeCounter == 3) { //back to life
+//                gc.setFill(Color.DIMGRAY);
+//                nextGeneration[x][y].setFill(Color.DIMGRAY);
+//            }
+//            else {
+//                gc.setFill(Color.WHITE);
+//                nextGeneration[x][y].setFill(Color.WHITE);
+//            }
+//        }
+//    }
 
 
 
