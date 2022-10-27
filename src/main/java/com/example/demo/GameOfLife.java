cb1a203fc58eb956b015a9c8482527d53b6d2ca4
Simha <mor.simha@intel.com>
2022-10-27 17:40:40 +0300

    finished_2

--- src/main/java/com/example/demo/GameOfLife.java
+++ src/main/java/com/example/demo/GameOfLife.java
@@ -1,3 +1,9 @@
+/**
+ * Maman 11, question 2.
+ * Mor Simha, 206029993.
+ */
+
+
 package com.example.demo;
 
 import javafx.application.Application;
@@ -11,8 +17,8 @@ public class GameOfLife extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameOfLife.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 503, 540);
+        stage.setTitle("Game of Life");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo/GameOfLifeController.java
+++ src/main/java/com/example/demo/GameOfLifeController.java
@@ -30,32 +30,6 @@ public class GameOfLifeController {
         outlineGc.setLineWidth(3);
     }
 
-    @FXML
-    protected void btnpressed() {
-        int lifeCounter;
-
-        for (int i = 0; i < SIDE; i++) {
-            for (int j = 0; j < SIDE; j++) {
-                if (firstRound) {
-                    matrix[i][j] = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-                    randomizeGame(i, j);
-                    gc.fillRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //inside color
-                } else {
-                    nextGeneration[i][j] = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-                    lifeCounter = calcNextGen(i, j);
-                    System.out.println("this is lifeCounter for (" +i+","+j +"): "+lifeCounter);
-                    paintNextGen(lifeCounter, i, j);
-                    gc.fillRect(nextGeneration[i][j].getX(), nextGeneration[i][j].getY(), nextGeneration[i][j].getWidth(), nextGeneration[i][j].getHeight()); //inside color
-                }
-                outlineGc.strokeRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //outline color
-            }
-        }
-        if (!firstRound)
-            for (int x = 0; x < SIDE; x++)
-                System.arraycopy(nextGeneration[x], 0, matrix[x], 0, SIDE);
-        firstRound = false;
-    }
-
     //Randomizes a number (0,1) and sets the color cell color by it.
     // WHITE is dead and DIMGRAY is alive
     private void randomizeGame(int x, int y) {
@@ -81,7 +55,6 @@ public class GameOfLifeController {
                     if (!(newX == x && newY == y)) // avoid check the middle one
                         if ((matrix[newX][newY].getFill() == Color.DIMGRAY)) {
                             lifeCounter++;
-                            System.out.println("cord - (" + x + " , " + y + ") and check:  (" + newX + ", " + newY + ")");
                         }
             }
         return lifeCounter;
@@ -101,31 +74,32 @@ public class GameOfLifeController {
             nextGeneration[x][y].setFill(Color.WHITE);
         }
     }
+    @FXML
+    protected void btnpressed() {
+        int lifeCounter;
+
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                if (firstRound) {
+                    matrix[i][j] = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    randomizeGame(i, j);
+                    gc.fillRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //inside color
+                } else {
+                    nextGeneration[i][j] = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    lifeCounter = calcNextGen(i, j);
+                    paintNextGen(lifeCounter, i, j);
+                    gc.fillRect(nextGeneration[i][j].getX(), nextGeneration[i][j].getY(), nextGeneration[i][j].getWidth(), nextGeneration[i][j].getHeight()); //inside color
+                }
+                outlineGc.strokeRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //outline color
+            }
+        }
+        if (!firstRound)
+            for (int x = 0; x < SIDE; x++)
+                System.arraycopy(nextGeneration[x], 0, matrix[x], 0, SIDE);
+        firstRound = false;
+    }
+
 }
-
-//
-//        if (currColor == Color.DIMGRAY) { //alive
-//            if (lifeCounter == 2 || lifeCounter == 3) {
-//                gc.setFill(Color.DIMGRAY);
-//                nextGeneration[x][y].setFill(Color.DIMGRAY);
-//            } else {
-//                gc.setFill(Color.WHITE);
-//                nextGeneration[x][y].setFill(Color.WHITE);
-//            }
-//        } else { //dead
-//            if (lifeCounter == 3) { //back to life
-//                gc.setFill(Color.DIMGRAY);
-//                nextGeneration[x][y].setFill(Color.DIMGRAY);
-//            }
-//            else {
-//                gc.setFill(Color.WHITE);
-//                nextGeneration[x][y].setFill(Color.WHITE);
-//            }
-//        }
-//    }
-
-
-
 
 
 
