6fae65a5a9a83c9b5c7aaea51f667a12f9e9ff42
Simha <mor.simha@intel.com>
2022-10-29 20:29:52 +0300

    final

--- src/main/java/com/example/demo/GameOfLife.java
+++ src/main/java/com/example/demo/GameOfLife.java
@@ -8,19 +8,17 @@ package com.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class GameOfLife extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameOfLife.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 525);
-        stage.setTitle("Game of Life");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Game of Life");
+        primaryStage.setScene(new Scene(root, 500, 525));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/GameOfLifeController.java
+++ src/main/java/com/example/demo/GameOfLifeController.java
@@ -1,3 +1,8 @@
+/**
+ * Maman 11, question 1.
+ * Mor Simha, 206029993.
+ */
+
 package com.example.demo;
 
 import javafx.fxml.FXML;
@@ -17,27 +22,52 @@ public class GameOfLifeController {
     Rectangle[][] nextGen = new Rectangle[MAT_SIZE][MAT_SIZE];
     private GraphicsContext gc;
 
-
     @FXML
     private Canvas cnvs;
 
     public void initialize() {
         gc = cnvs.getGraphicsContext2D();
         gc.setLineWidth(6);
+        drawMat();
+        firstRound = false;
+    }
+
+    private void drawMat() {
+        int lifeCounter;
+        Paint newColor;
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                Rectangle rect = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                if (firstRound) { //randomizing the first matrix
+                    matrix[i][j] = rect;
+                    newColor = randomizeGame();
+                    matrix[i][j].setFill(newColor);
+                } else { //Counting lives and calculating the next generation's matrix.
+                    nextGen[i][j] = rect;
+                    lifeCounter = countLives(i, j);
+                    newColor = CalculateNextGen(lifeCounter, i, j);
+                    nextGen[i][j].setFill(newColor);
+                }
+                gc.setFill(newColor); //choosing the right color for the cell
+                gc.fillRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //inside color
+                gc.strokeRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //outline color
+
+            }
+        }
     }
 
     //Randomizes a number (0,1) and sets the color cell color by it.
     // WHITE is dead and DIMGRAY is alive
-    private Paint randomizeGame(int x, int y) {
+    private Paint randomizeGame() {
         Random r = new Random();
-        if (r.nextInt(2) == 1) //Dead
-            return Color.WHITE;
-        else //Alive
+        if (r.nextInt(2) == 0) //Alive
             return Color.DIMGRAY;
+        else //Dead
+            return Color.WHITE;
     }
 
     // checks cell's sibling color, to count lives around it.
-    private int calcNextGen(int x, int y) {
+    private int countLives(int x, int y) {
         int lifeCounter = 0, newX, newY;
         // checks all siblings options, beside the out of range ones.
         for (int i = -1; i <= 1; i++)
@@ -53,7 +83,7 @@ public class GameOfLifeController {
         return lifeCounter;
     }
 
-    private Paint paintNextGen(int lifeCounter, int x, int y) {
+    private Paint CalculateNextGen(int lifeCounter, int x, int y) {
         Paint currColor = matrix[x][y].getFill();
         //3 siblings means a birth or existence for all cells,
         //2 siblings for a living cell means existence.
@@ -61,39 +91,14 @@ public class GameOfLifeController {
             return Color.DIMGRAY;
         else  //every other option means death
             return Color.WHITE;
-
     }
 
     @FXML
-    protected void btnpressed() {
-        int lifeCounter;
-        Paint newColor;
-
-        for (int i = 0; i < SIDE; i++) {
-            for (int j = 0; j < SIDE; j++) {
-                Rectangle rect =  new Rectangle(i*CELL_SIZE, j*CELL_SIZE, CELL_SIZE, CELL_SIZE);
-                if (firstRound) { //
-                    matrix[i][j] = rect;
-                //    matrix[i][j] = rect;
-                    newColor = randomizeGame(i, j);
-                    matrix[i][j].setFill(newColor);
-                } else {
-                    nextGen[i][j] = rect;
-                    lifeCounter = calcNextGen(i, j);
-                    newColor = paintNextGen(lifeCounter, i, j);
-                    nextGen[i][j].setFill(newColor);
-                }
-                gc.setFill(newColor);
-                gc.fillRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //inside color
-                gc.strokeRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //outline color
-            }
-        }
-        if (!firstRound)
-            for (int x = 0; x < SIDE; x++) //copy the new matrix to old one.
-                System.arraycopy(nextGen[x], 0, matrix[x], 0, SIDE);
-        firstRound = false;
+    private void btnPressed() {
+        drawMat();
+        for (int x = 0; x < SIDE; x++)
+            System.arraycopy(nextGen[x], 0, matrix[x], 0, SIDE); //TODO: check it out
     }
-
 }
 
 
