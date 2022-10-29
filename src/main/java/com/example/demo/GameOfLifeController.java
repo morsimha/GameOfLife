47928464faf58e6dfec4cb96bba01c83a011757d
Simha <mor.simha@intel.com>
2022-10-29 23:25:54 +0300

    classes version working

--- src/main/java/com/example/demo/GameOfLifeController.java
+++ src/main/java/com/example/demo/GameOfLifeController.java
@@ -8,94 +8,37 @@ package com.example.demo;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
-import java.util.Random;
 
 public class GameOfLifeController {
     @FXML
     private Canvas cnvs;
 
-    private final int MAT_SIZE = 100, SIDE = MAT_SIZE / 10, CELL_SIZE = 50;
-    private boolean firstRound = true;
-    private Rectangle[][] lastGenMat, nextGenMat;
+    private final int SIDE = 10;
     private GraphicsContext gc;
+    GameOfLifeLogic game = new GameOfLifeLogic();
 
     public void initialize() {
         gc = cnvs.getGraphicsContext2D();
         gc.setLineWidth(6);
-        lastGenMat = new Rectangle[MAT_SIZE][MAT_SIZE];
-        nextGenMat = new Rectangle[MAT_SIZE][MAT_SIZE];
-        drawMat(lastGenMat);
-        firstRound = false;
+        game.createGeneration();
+        fillRects();
+        // game.getMat().getFill
     }
+
     @FXML
     private void btnPressed() {
-        drawMat(nextGenMat);
-        for (int x = 0; x < SIDE; x++)
-            System.arraycopy(nextGenMat[x], 0, lastGenMat[x], 0, SIDE);
-    }
-    //Randomizes or calculates every generation's matrix.
-    private void drawMat(Rectangle[][] matrix) {
-        int lifeCounter;
-        Paint newColor;
-        for (int i = 0; i < SIDE; i++) {
-            for (int j = 0; j < SIDE; j++) {
-                Rectangle rect = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-                matrix[i][j] = rect;
-                if (firstRound) //randomizing the first matrix
-                    newColor = randomizeGame();
-                else { //Counting lives and calculating the next generation's matrix.
-                    lifeCounter = countLives(i, j);
-                    newColor = CalculateNextGen(lifeCounter, i, j);
-                }
-                matrix[i][j].setFill(newColor);
-                fillRects(matrix,newColor,i,j);
-            }
-        }
-    }
-
-    //Randomizes a number (0,1) and sets the color cell color by it.
-    // WHITE is dead and DIMGRAY is alive
-    private Paint randomizeGame() {
-        Random r = new Random();
-        if (r.nextInt(2) == 0) //Alive
-            return Color.DIMGRAY;
-        else //Dead
-            return Color.WHITE;
-    }
-
-    // checks cell's sibling color, to count lives around it.
-    private int countLives(int x, int y) {
-        int lifeCounter = 0, newX, newY;
-        // checks all siblings options, beside the out of range ones.
-        for (int i = -1; i <= 1; i++)
-            for (int j = -1; j <= 1; j++) {
-                newX = x + i; //new x coordinate to check
-                newY = y + j;
-                if (((newX >= 0 && newY >= 0) && (newX < SIDE && newY < SIDE)) && !(newX == x && newY == y)) // check positive and not the middle coordinate
-                    if ((lastGenMat[newX][newY].getFill() == Color.DIMGRAY)) // avoid checking the middle one
-                        lifeCounter++;
-            }
-        return lifeCounter;
-    }
-    //returns the correct cell color, according to its siblings.
-    private Paint CalculateNextGen(int lifeCounter, int x, int y) {
-        Paint currColor = lastGenMat[x][y].getFill();
-        //3 siblings means a birth or existence for all cells,
-        //2 siblings for a living cell means existence.
-        if (lifeCounter == 3 || (lifeCounter == 2 && currColor == Color.DIMGRAY))
-            return Color.DIMGRAY;
-        else  //every other option means death
-            return Color.WHITE;
+        game.createGeneration();
+        fillRects();
     }
 
     //filling the rectangles according to the right color.
-    private void fillRects(Rectangle[][] matrix, Paint color,int i, int j){
-        gc.setFill(color); //choosing the right color for the cell
-        gc.fillRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //filling inside color
-        gc.strokeRect(matrix[i][j].getX(), matrix[i][j].getY(), matrix[i][j].getWidth(), matrix[i][j].getHeight()); //filling outline color
+    private void fillRects() {
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                gc.setFill(game.getMat()[i][j].getFill()); //choosing the right color for the cell
+                gc.fillRect(game.getMat()[i][j].getX(), game.getMat()[i][j].getY(), game.getMat()[i][j].getWidth(), game.getMat()[i][j].getHeight()); //filling inside color
+                gc.strokeRect(game.getMat()[i][j].getX(), game.getMat()[i][j].getY(), game.getMat()[i][j].getWidth(), game.getMat()[i][j].getHeight()); //filling outline color
+            }
+        }
     }
-
 }
---
+++ src/main/java/com/example/demo/GameOfLifeLogic.java
@@ -0,0 +1,90 @@
+package com.example.demo;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import java.util.Random;
+
+public class GameOfLifeLogic {
+
+    private final int MAT_SIZE = 100, SIDE = (MAT_SIZE / 10), CELL_SIZE = 50;
+    private boolean firstRound;
+    private Rectangle[][] historyMat, GenerationMat; //history will always hold last generation's matrix.
+
+    public GameOfLifeLogic() {
+        this.historyMat = new Rectangle[MAT_SIZE][MAT_SIZE];
+        this.GenerationMat = new Rectangle[MAT_SIZE][MAT_SIZE];
+        this.firstRound = true;
+    }
+
+    public Rectangle[][] getMat() {
+        return GenerationMat;
+    }
+
+    //Randomizes or calculates generation's matrix.
+    public void createGeneration() {
+        int lifeCounter;
+        Paint newColor;
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                Rectangle rect = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                GenerationMat[i][j] = rect;
+                if (firstRound) //randomizing the first matrix
+                    newColor = randomizeGame();
+                else { //Counting lives and calculating the next generation's matrix.
+                    lifeCounter = countLives(i, j);
+                    newColor = CalculateNextGen(lifeCounter, i, j);
+                }
+                GenerationMat[i][j].setFill(newColor);
+            }
+        }
+        copyMat();
+        if (firstRound)
+            firstRound = false;
+    }
+
+
+    //Copies the current matrix to the history matrix.
+    private void copyMat() {
+        for (int i = 0; i < SIDE; i++)
+            for (int j = 0; j < SIDE; j++)
+                historyMat[i][j] = GenerationMat[i][j];
+    }
+
+    //Randomizes a number (0,1) and sets the color cell color by it.
+    // WHITE is dead and DIMGRAY is alive
+    private Paint randomizeGame() {
+        Random r = new Random();
+        if (r.nextInt(2) == 0) //Alive
+            return Color.DIMGRAY;
+        else //Dead
+            return Color.WHITE;
+    }
+
+    // Checks cell's sibling color, to count lives around it.
+    private int countLives(int x, int y) {
+        int lifeCounter = 0, newX, newY;
+        // checks all siblings options, beside the out of range ones.
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++) {
+                newX = x + i; //new x coordinate to check
+                newY = y + j;
+                if (((newX >= 0 && newY >= 0) && (newX < SIDE && newY < SIDE)) && !(newX == x && newY == y)) // check positive and not the middle coordinate
+                    if ((historyMat[newX][newY].getFill() == Color.DIMGRAY)) // avoid checking the middle one
+                        lifeCounter++;
+            }
+        return lifeCounter;
+    }
+
+    //Returns the correct cell color, according to its siblings.
+    private Paint CalculateNextGen(int lifeCounter, int x, int y) {
+        Paint currColor = historyMat[x][y].getFill();
+        //3 siblings means a birth or existence for all cells,
+        //2 siblings for a living cell means existence.
+        if (lifeCounter == 3 || (lifeCounter == 2 && currColor == Color.DIMGRAY))
+            return Color.DIMGRAY;
+        else  //every other option means death
+            return Color.WHITE;
+    }
+
+}
